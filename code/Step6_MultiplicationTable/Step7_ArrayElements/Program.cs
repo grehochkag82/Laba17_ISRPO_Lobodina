368ec4992aaacd724621e4b6901a300252bb2c07
grehochka82 <lobodina0607@mail.ru>
2026-03-10 13:27:06 +0300

    Step 7 arrays

---
+++ code/Step6_MultiplicationTable/Step7_ArrayElements/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Вывод элементов массива");
+int[] grades = { 5, 4, 5, 3, 4 };
+Console.WriteLine("Массив оценок студента:");
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.WriteLine($"Элемент с индексом [{i}] = {grades[i]}");
+}
+Console.WriteLine("\nКонец программы.");
