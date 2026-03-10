4551ad8aaa9769b2fe7f4a3fbe5db4c2ae798714
grehochka82 <lobodina0607@mail.ru>
2026-03-10 14:07:25 +0300

    Step 8: functions and modular programming

---
+++ code/Step8_FunctionCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Step8_FunctionCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Калькулятор (с использованием функций)");
+            Console.Write("Введите первое число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"\nРезультаты:");
+            Console.WriteLine($"{num1} + {num2} = {Sum(num1, num2)}");
+            Console.WriteLine($"{num1} - {num2} = {Subtract(num1, num2)}");
+            Console.WriteLine($"{num1} * {num2} = {Multiply(num1, num2)}");
+            if (num2 != 0)
+            {
+                Console.WriteLine($"{num1} / {num2} = {Divide(num1, num2)}");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} / {num2} = Ошибка: деление на ноль!");
+            }
+
+            Console.WriteLine("\nКонец программы.");
+        }
+        static double Sum(double a, double b)
+        {
+            double result = a + b;
+            return result;
+        }
+        static double Subtract(double a, double b)
+        {
+            return a - b; 
+        }
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
